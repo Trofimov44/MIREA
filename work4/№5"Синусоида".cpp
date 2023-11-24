5d121b4ff57a16ec314e314f231dc6f3dd7053dc
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2023-11-24 18:04:33 +0300

    Update №5"Синусоида".cpp

--- work4/№5"Синусоида".cpp
+++ work4/№5"Синусоида".cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+int main() {
+    const int width = 80; // Ширина экрана
+    const int height = 20; // Высота экрана
+    const double amplitude = height / 2.0; // Амплитуда синусоиды
+    const double frequency = 0.1; // Частота синусоиды
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            double value = amplitude * sin(frequency * x);
+            if (y == height / 2) {
+                cout << "-";
+            }
+            else if ((x == 0 && y == amplitude) || (x == width - 1 && y == amplitude)) {
+                cout << "+";
+            }
+            else if (x == static_cast<int>(frequency * x) || x == static_cast<int>(frequency * x) + width / 2) {
+                cout << "|";
+            }
+            else {
+                cout << (y == static_cast<int>(amplitude - value) ? "*" : " ");
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
