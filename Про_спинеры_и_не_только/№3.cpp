034c574cfd80d58ec57a1eb2562e6153ff849674
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2023-11-30 18:06:52 +0300

    Update №3.cpp

--- Про_спинеры_и_не_только/№3.cpp
+++ Про_спинеры_и_не_только/№3.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	setlocale(0, "");
+		cout <<"Программа находит количество прямоугольников, которые можно вырезать из листа" << endl;
+		cout << "Введите 2 параметры листа" << endl;
+	int n, m;
+	if ((cin >> n && cin >> m)and(m != '.' && m != ',' && n!= '.' && n!= ','))
+	{
+		cout << n * (n + 1) / 2 * m * (m + 1) / 2;
+	}
+	else
+	{
+		cout << "Введите целые положительные числа";
+	}
+	return 0;
+}
