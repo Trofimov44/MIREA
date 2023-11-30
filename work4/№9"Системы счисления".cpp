7ab7d5d6014c3d12db2ebbd06e5bac015f9bb0a3
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2023-11-30 14:31:06 +0300

    Update №9"Системы счисления".cpp

--- work4/№9"Системы счисления".cpp
+++ work4/№9"Системы счисления".cpp
@@ -1,79 +1,64 @@
 #include <iostream>
-#include <cmath>
 #include <string>
-#include <fstream>
 using namespace std;
 
-int Hex(char HexNum) {  
-  switch (HexNum) {
-  case '0': return 0;
-  case '1': return 1;
-  case '2': return 2;
-  case '3': return 3;
-  case '4': return 4;
-  case '5': return 5;
-  case '6': return 6;
-  case '7': return 7;
-  case '8': return 8;
-  case '9': return 9;
-  case 'A': return 10;
-  case 'B': return 11;
-  case 'C': return 12;
-  case 'D': return 13;
-  case 'E': return 14;
-  case 'F': return 15;
-  default: return -1;
-  }
-}
-
-string decToAny(int decim, int a) {
-  string octal = "";
-  while (decim > 0) {
-    int rem = decim % a;
-    octal = to_string(rem) + octal;
-    decim /= a;
-  }
-  return octal;
-}
-
-int main() {
-  setlocale(LC_ALL, "rus");
-  string HexNum;
-  int num;
-  int syst;
-  bool schet = 0;
-  bool f1 = 0;
-
-  cout << "Введите число: ";
-  cin >> HexNum;
-
-  cout << "Введите систему счисления вашего числа: ";
-  cin >> syst;
-  if (syst > 16) {
-    cout << "Вы ввели некорректное значение!" << endl;
-    return 0;
-  }
-
-  cout << "Введите систему счисления для перевода (от 2 до 16): ";
-  cin >> num;
-  if (num > 16) {
-    cout << "Вы ввели некорректное значение!" << endl;
-    return 0;
-  }
-
-  int decim = 0;
-
-  for (int i = 0; i < HexNum.length(); i++) {
-    int value = Hex(HexNum[i]);
-    if (value == -1) {
-      cout << "Некорректное число." << endl;
-      return 0;
+int Hex(char HexNum) {
+    switch (HexNum) {
+    case '0': return 0;
+    case '1': return 1;
+    case '2': return 2;
+    case '3': return 3;
+    case '4': return 4;
+    case '5': return 5;
+    case '6': return 6;
+    case '7': return 7;
+    case '8': return 8;
+    case '9': return 9;
+    case 'A': return 10;
+    case 'B': return 11;
+    case 'C': return 12;
+    case 'D': return 13;
+    case 'E': return 14;
+    case 'F': return 15;
+    default: return -1;
     }
-    decim = decim * syst + value;
-  }
+}
 
-  string result = decToAny(decim, num);
-  cout << "Число " << HexNum << " в системе счисления с основанием " << num << " равно " << result << "." << endl;
+int Ten(int a, int b) {
+    if (b > 16) {
+        cout << "Ошибка" << endl;
+    }
 
-  return 0;
+    else {
+        int y = 0;
+        string c = to_string(a);
+        for (int i = 0; i < c.length(); i++) {
+            char pipa = c[i];
+            int biba = static_cast<int>(c.length());
+            int x = Hex(pipa) * pow(b, biba - i-1);
+            y += x;
+        }
+        return y;
+    }   
+}
+
+string Any(int a, int b, int c) {
+    int x = Ten(a, b);
+    string y = "";
+    while (x > 0) {
+        int pipa = x % c;
+        y = to_string(pipa) + y;
+        x /= c;
+    }
+    return y;
+}
+
+int main()
+{
+    int a, b, c;
+    if (!(cin >> a >> b >> c)) {
+        cout << "Ошибка" << endl;
+    }
+    cout << Any(a, b, c) << endl;
+    return 0;
 }
