c43ce5384f2c2bf1e289e2849c561542190dfbd0
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2023-12-01 19:04:32 +0300

    Update Шарики.cpp

--- Шарики/Шарики.cpp
+++ Шарики/Шарики.cpp
@@ -1,10 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+void print_permutation(int permutation[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << permutation[i] << " ";
+    }
+    cout << endl;
+}
+
+void generate_permutation(int permutation[], int n, int i, int& count, vector<int*>& valid_permutations) {
+    if (i == n) {
+        bool has_match = false;
+        for (int j = 0; j < n; j++) {
+            if (permutation[j] == j + 1) {
+                has_match = true;
+                break;
+            }
+        }
+
+        if (has_match) {
+            count++;
+            int* valid_permutation = new int[n];
+            copy(permutation, permutation + n, valid_permutation);
+            valid_permutations.push_back(valid_permutation);
+        }
+
+        return;
+    }
+
+    for (int j = i; j < n; j++) {
+        swap(permutation[i], permutation[j]);
+
+        generate_permutation(permutation, n, i + 1, count, valid_permutations);
+
+        swap(permutation[i], permutation[j]);
+    }
+}
+
 int main() {
+    setlocale(LC_ALL, "Rus");
     while (true) {
-        //добавить ввод проверку
 
         int n;
         while (true) {
-            cout << "enter the number of balls: ";
+            cout << "Введите количество шаров: ";
             if (!(cin >> n)) {
                 cin.clear();
                 cin.ignore();
@@ -24,24 +66,14 @@ int main() {
 
         generate_permutation(balls, n, 0, count_matches, valid_permutations);
 
-        //cout << "\npermutations with at least one match:\n";
-        //for (int i = 0; i < valid_permutations.size(); i++) {
-        //    print_permutation(valid_permutations[i], n);
-        //}
-
-        cout << "\ntotal number of permutations with at least one match: " << count_matches;
-
-        /*cout << "\nall permutations:\n";
-        do {
-            print_permutation(balls, n);
-        } while (next_permutation(balls, balls + n));*/
+        cout << "\nОбщее количество перестановок хотя бы с одним совпадением: " << count_matches;
 
         int total_permutations = 1;
         for (int i = 1; i <= n; i++) {
             total_permutations *= i;
         }
 
-        cout << "\ntotal permutations: " << total_permutations << endl;
+        cout << "\nВсего перестановок:" << total_permutations << "\n" << endl;
 
         for (int i = 0; i < valid_permutations.size(); i++) {
             delete[] valid_permutations[i];
