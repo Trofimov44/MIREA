c65bdde3b81926fac896bb1a09e8ba7afde5063b
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2023-12-01 17:37:30 +0300

    Update №8"Умножение матриц".cpp

--- work4/№8"Умножение матриц".cpp
+++ work4/№8"Умножение матриц".cpp
@@ -1,33 +1,83 @@
-#include <iostream>
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<string>
+
 using namespace std;
+
 int main() {
-    setlocale(LC_ALL, "RUS");
-    float matrix1[3][4] = { {5., 2., 0, 10.},
-                           {3., 5., 2., 5.},
-                           {20., 0, 0, 0} };
 
-    float matrix2[4][2] = { {1.20, 0.50},
-                           {2.80, 0.40},
-                           {5., 1.},
-                           {2., 1.5} };
+    const int seller_string = 3;
+    const int product_column = 4;
 
-    float result[3][2] = { 0 };
+    const int price_string = 4;
+    const int commission_column = 2;
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
-            for (int k = 0; k < 4; k++) {
-                result[i][j] += matrix1[i][k] * matrix2[k][j];
+    double table_A[seller_string][product_column]{ {5, 2, 0, 10}, {3, 5, 2, 5}, {20, 0, 0, 0} };
+    double table_B[price_string][commission_column]{ {1.20, 0.50}, {2.80, 0.40}, {5.00, 1.00}, {2.00, 1.50} };
+
+
+    double multiplication_result[seller_string][commission_column] = {};
+    for (int i = 0; i < seller_string; i++) {
+        for (int j = 0; j < commission_column; j++) {
+            for (int k = 0; k < product_column; k++) {
+                multiplication_result[i][j] += table_A[i][k] * table_B[k][j];
             }
         }
     }
-
-    cout << "Результат умножения матриц:" << endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
-            std::cout << result[i][j] << " ";
-        }
-        cout << endl;
+    if (product_column != price_string) {
+        cout << "error \n"; //проверка на возможность перемножения матриц
+        return 1;
     }
 
-    return 0;
+
+    double total_sales = 0;
+    double total_commission = 0;
+    double max_sales = multiplication_result[0][0];
+    double min_sales = multiplication_result[0][0];
+    double max_commission = multiplication_result[0][1];
+    double min_commission = multiplication_result[0][1];
+
+    int max_sales_seller = 0;
+    int min_sales_seller = 0;
+    int max_commission_seller = 0;
+    int min_commission_seller = 0;
+
+    for (int i = 0; i < seller_string; i++) {
+        total_sales += multiplication_result[i][0];
+        total_commission += multiplication_result[i][1];
+
+        if (multiplication_result[i][0] > max_sales) {
+            max_sales = multiplication_result[i][0];
+            max_sales_seller = i;
+        }
+        if (multiplication_result[i][0] < min_sales) {
+            min_sales = multiplication_result[i][0];
+            min_sales_seller = i;
+        }
+
+        if (multiplication_result[i][1] > max_commission) {
+            max_commission = multiplication_result[i][1];
+            max_commission_seller = i;
+        }
+        if (multiplication_result[i][1] < min_commission) {
+            min_commission = multiplication_result[i][1];
+            min_commission_seller = i;
+        }
+    }
+    for (int i = 0; i < seller_string; i++) {
+        for (int j = 0; j < commission_column; j++) {
+            cout << multiplication_result[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << endl;
+
+    cout << "1) "<< max_sales << min_sales << endl;
+    cout << "2)" << max_commission << min_commission << endl;
+    cout << "3)" << total_sales << endl;
+    cout << "4)" << total_commission << endl;
+    cout << "5)" << total_sales + total_commission << endl << endl;
+
+
 }
