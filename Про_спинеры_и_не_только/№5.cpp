ba6155f73a9d9fa8a7be19d2fbe43fe39e71bb6b
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2023-12-01 22:45:49 +0300

    Update №5.cpp

--- Про_спинеры_и_не_только/№5.cpp
+++ Про_спинеры_и_не_только/№5.cpp
@@ -2,23 +2,22 @@
 using namespace std;
 
 int main() {
-	setlocale(0, "");
-	cout << "Программа выводит количество свободных мест с двух сторон от школьник, который последним занял место в ряду" << endl;
-	cout << "Введите кол-во свободных мест и какой по счету школьник пришел" << endl;
-	int n;
-	long long n, m;
-	if ((cin >> n >> m) and (m != '.' && m != ',' && n != '.' && n != ','))
-	{
-		while (m != 1)
-		{
-			n = (n - m % 2) / 2;
-			m /= 2;
-			cout << (n - 1) / 2 << endl << n / 2;
-		}
-	}
-	else
-	{
-		cout << "Введите 2 положительных целых числа";
-	}
-	return 0;
+    int n, k;
+
+    cout << "enter : ";
+    if (cin >> n >> k && k > 1 && k <= n) {
+        while (k > 1) {
+            n = (n - k % 2) / 2;
+            k /= 2;
+        }
+
+        int left_seats = (n - 1) / 2;
+        int right_seats = n / 2;
+
+        cout << left_seats << endl;
+        cout << right_seats << endl;
+    }
+    else {
+        cout << "error! ";
+    }
 }
