faa7bdcb7daed70ddfae76e04596785c8f085057
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2023-12-04 00:10:21 +0300

    Update №4.1"Ряды".cpp

--- work5/№4.1"Ряды".cpp
+++ work5/№4.1"Ряды".cpp
@@ -6,7 +6,12 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "rus");
-	int n = 2;
+	int n;
+	int a, b;
+	if (!(cin >> n)) {
+		cout << "Неверно";
+		return 0;
+	}
 	double x = 0, c = 0, y = 0;
 
 	for (int i = 1; i < n + 1; i++) {
