5c62f47a1cb2bd430258f37ac9940404e0463c65
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2023-12-03 22:56:37 +0300

    Update №2"Решето Эратосфена".cpp

--- work5/№2"Решето Эратосфена".cpp
+++ work5/№2"Решето Эратосфена".cpp
@@ -1,1 +1,43 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+
+void da(int n, int m, int x[]) {
+	setlocale(LC_ALL, "rus");
+	if (m < n) {
+		if (x[m] != 0) { 
+			int j = m * 2;
+			while (j < n) {
+				x[j] = 0; 
+				j = j + m;
+			}
+			m += 1;
+			da(n, m, x);
+		}
+		else {
+			m += 1;
+			da(n, m, x);
+		}
+	}
+	else {
+		for (int i = 0; i < n - 1; i++) {
+			if (x[i] != 0 and x[i] != 1) {
+				cout << x[i] << endl;
+			}
+		}
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n = 124;
+	int a = n - 1;
+	int m = 2;
+	int* x = new int[a];
+	for (int i = 0; i < a; i++) {
+		x[i] = i;
+	}
+	da(n, m, x);
+	return 0;
+}
