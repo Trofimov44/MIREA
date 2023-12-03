d450f8cba1b594ec05928337aedd561a3c139ddb
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2023-12-03 23:39:51 +0300

    Update №4.1"Ряды".cpp

--- work5/№4.1"Ряды".cpp
+++ work5/№4.1"Ряды".cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n = 2;
+	double x = 0, c = 0, y = 0;
+
+	for (int i = 1; i < n + 1; i++) {
+		x = x + sin(i);
+		c = i / x;
+		y += c;
+	}
+	cout << y << endl;
+	return 0;
+}
