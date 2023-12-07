f2d3f291805279ba988a27682c42195984028093
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2023-12-07 22:48:37 +0300

    Update and rename №4.2"Ряды".cpp to №4.19"Ряды".cpp

---
+++ work5/№4.19"Ряды".cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    float a = 1, v = 0, t = 0;
+    int c = -1;
+    vector<float> x;
+    cout << "/**** Анализ роста учеников ***" << endl;
+    cout << "Введите рост (см) и нажмите <Enter>" << endl;
+
+    while (a != 0) {
+        cout << "->";
+        if (!(cin >> a)) {
+            cout << "Ошибка" << endl;
+            return 0;
+        }
+        x.push_back(a);
+    }
+    for (int i : x) {
+        c += 1;
+        v += i;
+    }
+    v = v / c;
+    for (int i : x) {
+        if (i > v) {
+            t += 1;
+        }
+    }
+
+    cout << "Средний рост: " << v << endl;
+    cout << "У " << t << " человек рост превышает средний*/";
+    return 0;
+}
