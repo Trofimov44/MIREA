d72a4a5af056f4e600011bd1af127c4babf9e0d2
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2023-12-08 00:14:02 +0300

    Update and rename №3.1"Обработка текстовых файлов".cpp to №3.2"Обработка текстовых файлов".cpp

---
+++ work5/№3.2"Обработка текстовых файлов".cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    ifstream inputFile("text.txt"); 
+
+    if (!inputFile.is_open()) {
+        cerr << "Не удалось открыть файл." << endl;
+        return 1;
+    }
+
+    string line;
+
+    // Считывание строки из файла
+    if (getline(inputFile, line)) {
+        for (int i = 0; i < line.size(); ++i) {
+            if (line[i] == ' ') {
+                line[i] = '\n';
+            }
+        }
+        cout << line;
+    }
+    else {
+        cerr << "Не удалось прочитать строку из файла." << endl;
+    }
+
+    inputFile.close();  // Закрытие файла
+
+    return 0;
+}
