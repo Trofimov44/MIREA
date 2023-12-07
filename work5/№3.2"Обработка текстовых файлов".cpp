70cd5e02868ab4bd59fe73327d99c3a27aad3258
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2023-12-08 00:25:05 +0300

    Update №3.2"Обработка текстовых файлов".cpp

--- work5/№3.2"Обработка текстовых файлов".cpp
+++ work5/№3.2"Обработка текстовых файлов".cpp
@@ -13,7 +13,6 @@ int main() {
 
     string line;
 
-    // Считывание строки из файла
     if (getline(inputFile, line)) {
         for (int i = 0; i < line.size(); ++i) {
             if (line[i] == ' ') {
