44b143b9d6c8e86aabefed9769ddb2d72c695c3d
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2023-12-08 16:54:02 +0300

    Update №5.1"Файлы".cpp

--- work5/№5.1"Файлы".cpp
+++ work5/№5.1"Файлы".cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int main() {
+    string filename = "word_data.txt";
+    vector<string> words;
+
+    ifstream inFile(filename);
+    if (inFile.is_open()) {
+        string word;
+        while (inFile >> word) {
+            words.push_back(word);
+        }
+        inFile.close();
+    }
+    else {
+        cerr << "Ошибка при открытии файла для чтения." << endl;
+        return 1;
+    }
+
+    string newWord;
+    cout << "Введите новое слово: ";
+    cin >> newWord;
+    words.push_back(newWord);
+
+    sort(words.begin(), words.end());
+
+    std::ofstream outFile(filename);
+    if (outFile.is_open()) {
+        for (const auto& word : words) {
+            outFile << word << endl;
+        }
+        outFile.close();
+        cout << "Данные были сохранены в " << filename << endl;
+    }
+    else {
+        cerr << "Ошибка при открытии файла для записи." << endl;
+        return 1;
+    }
+
+    return 0;
+}
