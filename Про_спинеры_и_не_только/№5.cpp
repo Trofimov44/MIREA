c4583601ab3759f7c25c54f81773b0a49115d449
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2023-12-08 18:46:19 +0300

    Update №5.cpp

--- Про_спинеры_и_не_только/№5.cpp
+++ Про_спинеры_и_не_только/№5.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 int main() {
-    int n, k;
+        int n, k;
 
-    cout << "Ведите N и K : ";
-    if (cin >> n >> k and k > 1 and k <= n) {
-        while (k > 1) {
-            n = (n - k % 2) / 2;
-            k /= 2;
+        cout << "Введите N и K";
+        if (cin >> n >> k && k >= 1 && k <= n) {
+            while (k >= 1) {
+                n = (n - k % 2) / 2;
+                k /= 2;
+            }
+
+            int left_seats = (n - 1) / 2;
+            int right_seats = n / 2;
+
+            cout << left_seats << endl;
+            cout << right_seats << endl;
         }
-
-        int left_seats = (n - 1) / 2;
-        int right_seats = n / 2;
-
-        cout << left_seats << endl;
-        cout << right_seats << endl;
+        else {
+            cout << "Ошибка ";
+        }
     }
-    else {
-        cout << "error! ";
-    }
-}
